a53ec93878548e1ec8d3529f1bd086f3a50f4269
hobbyquaker <hobbyquaker@gmail.com>
2025-05-15 17:20:57 +0200

    add alias "off" for "black"

--- ss-rgb/CssColorNames.cs
+++ ss-rgb/CssColorNames.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 static class CssColorNames {
     public static readonly Dictionary<string, int> Map = new(StringComparer.OrdinalIgnoreCase) {
-        { "black", 0x000000 }, { "silver", 0xC0C0C0 }, { "gray", 0x808080 }, { "white", 0xFFFFFF },
+        { "off", 0x000000 }, { "black", 0x000000 }, { "silver", 0xC0C0C0 }, { "gray", 0x808080 }, { "white", 0xFFFFFF },
         { "maroon", 0x800000 }, { "red", 0xFF0000 }, { "purple", 0x800080 }, { "fuchsia", 0xFF00FF },
         { "green", 0x008000 }, { "lime", 0x00FF00 }, { "olive", 0x808000 }, { "yellow", 0xFFFF00 },
         { "navy", 0x000080 }, { "blue", 0x0000FF }, { "teal", 0x008080 }, { "aqua", 0x00FFFF },
